a891c410a0bd229d93720cf4b13ab99451d5cdd4
chenjian <jsdtcj@126.com>
2019-06-20 11:19:32 +0800

    史蒂夫

--- config/loaders/style-loaders/index.js
+++
@@ -1,8 +0,0 @@
-var utils=require('./utils.js')
-module.exports=function(conf){
-	return utils.styleLoaders({
-		sourceMap: true,
-		extract: true,
-		usePostCSS:conf.postCss
-	},conf)
-};
--- config/pot.js
+++
@@ -1,4 +0,0 @@
-process.env.NODE_ENV='development';
-const developBase=require('./development.base.js');
-new developBase('pot_components/example')
-new developBase('pot_components/document');
